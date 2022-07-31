9bd4bfdc7951e83eb4140ec3fe336f1aaa83ab9a
Ameer Hamza Subhani <Subhania895@gmail.com>
2022-07-31 21:28:42 +0500

    sigin in and signp

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,9 @@ import './App.css';
 import LandingPage from './Screens/landingpage/LandingPage';
 import { Routes ,Route} from 'react-router-dom';
 import VenueDetialsPage from "./Components/VenueDetialsPage"
+import SignIn from './Screens/SignIn';
+import SignupOptions from './Screens/SignupOptions';
+import SignUp from './Screens/SignUp';
 function MainRoutes() {
   return (
     <div className="Routes">
@@ -11,6 +14,12 @@ function MainRoutes() {
       <Route path="/" element={<LandingPage />}>
          </Route>
         <Route path="/decordetails" element={<VenueDetialsPage />} />
+        <Route path="/Signin" element={<SignIn />} />
+        <Route path="/Signup" element={<SignUp/>} />
+
+        <Route path="/Signupoptions" element={<SignupOptions />} />
+
+
 
     </Routes>
     </div>
